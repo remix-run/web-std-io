1a2c2bf2744ccc7d87eb3f575042d31cf7b1aab0
Jacob Ebey <jacob.ebey@live.com>
2022-04-27 11:12:03 -0700

    add file and form-data to always be polyfilled

--- packages/form-data/src/lib.node.js
+++ packages/form-data/src/lib.node.js
@@ -5,7 +5,4 @@ import * as polyfill from "./form-data.js"
 
 // Electron-renderer should get the browser implementation instead of node
 // which is why we check global first.
-export const FormData =
-  typeof globalThis.FormData === "function"
-    ? globalThis.FormData
-    : polyfill.FormData
+export const FormData = polyfill.FormData
