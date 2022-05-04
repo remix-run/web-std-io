a189fb257df71479a3b1fccf9bdf6de5fea31114
Jacob Ebey <jacob.ebey@live.com>
2022-05-03 21:56:02 -0700

    fix: proper exports to allow TS to recognize class types and proper casting

--- packages/fetch/src/lib.node.js
+++ packages/fetch/src/lib.node.js
@@ -1,15 +1,6 @@
-import * as WebFetch from "./fetch.js"
+export { default, Headers, Request, Response } from "./fetch.js"
 
-export { ReadableStream, Blob, FormData  } from './package.js';
 // Node 18+ introduces fetch API globally and it doesn't support our use-cases yet.
 // For now we always use the polyfill.
+export { ReadableStream, Blob, FormData  } from './package.js'
 
-// Marking export as a DOM File object instead of custom class.
-export const fetch = /** @type {typeof globalThis.fetch} */
-  WebFetch.fetch
-
-export const Headers = WebFetch.Headers
-export const Request = WebFetch.Request
-export const Response = WebFetch.Response
-
-export default fetch
